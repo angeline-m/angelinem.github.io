2291392e356171aa80c75e3fe4b7e5ab276baddf
unknown <shelite17@gmail.com>
2022-03-01 14:33:13 -0700

    Updated about page carousel settings

--- js/main.js
+++ js/main.js
@@ -57,6 +57,7 @@ $(document).ready(function(){
         speed: 9000,
         autoplay: true,
         autoplaySpeed: 0,
+        cssEase: 'linear',
         slidesToShow: 1,
         slidesToScroll: 1,
         variableWidth: true,
